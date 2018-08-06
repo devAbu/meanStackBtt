a581f7791743ca3cc7367294564a0d9c875e4ccd
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-06 18:57:42 +0200

    express static

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use('/', function(req, res){
-    res.send("Hello World")
-})
+app.use('/', express.static(__dirname + '/static'));
 
 app.listen(3000, function () {
     console.log('Running on port 3000')
