9ece7736fb32b123b8192c574a151b3e63283d20
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-08 23:04:26 +0200

    heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express()
 const port = process.env.port || '3000';
 
 app.use('/', express.static(__dirname + '/static'));
+app.get('/', (req, res) => res.sendStatus(200));
 
 app.listen(port, function () {
 	console.log('3000 port');
