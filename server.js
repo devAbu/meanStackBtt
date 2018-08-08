e324189ca10410080f9dab8b612b4eb8f729164b
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-08 23:06:55 +0200

    .

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/static'));
 
-const port = process.env.port || '3000';
-
-app.use('/', express.static(__dirname + '/static'));
 app.get('/', (req, res) => res.sendStatus(200));
-
-app.listen(port, function () {
-	console.log('3000 port');
-})
-
-//test
+app.listen(port,function(){
+  console.log('Node app is running on port', port);
+});
