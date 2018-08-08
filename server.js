0fce585e9cdee3e5cb71d862bc3ec3ee94da72cd
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-08 19:16:06 +0200

    update

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 3000))
+
+const port = process.env.port || '3000';
 
 app.use('/', express.static(__dirname + '/static'));
 
-app.listen(app.get('port'), function () {
+app.listen(port, function () {
 	console.log('3000 port');
 }
