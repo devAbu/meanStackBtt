2457bca0764de2e411910bb17da3704ee2d65e9e
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-08 23:14:40 +0200

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => res.sendStatus(200));
+
 app.listen(port,function(){
   console.log('Node app is running on port', port);
 });
