3761561b15a5159151488a4fd935caf50a16c838
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-09 11:06:44 +0200

    mongojs and body-parser

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/static'));
+var mongojs = require('mongojs')
+var db = mongojs('localhost:27017/btt', ['tour'])
 
+var body_parser = require('body-parser')
+app.use(body_parser.json())
 
-app.listen(port,function(){
-  console.log('Node app is running on port', port);
-});
+app.use(express.static(__dirname + '/static'))
+
+app.get('/tour', function (req, res) {
+  db.tour.find(function (err, docs) {
+    res.json(docs)
+  })
+})
+
+app.listen(port, function () {
+  console.log('Node app is running on port', port)
+})
