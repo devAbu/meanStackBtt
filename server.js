5afd4b90c06ea38399eaa5803f579fe2bd3e45ad
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-25 19:12:27 +0200

    pokusaj joina (lookup - aggregation)

--- server.js
+++ server.js
@@ -22,18 +22,20 @@ var urlencodedParser = bodyparser.urlencoded({
   extended: false
 })
 
-app.use('/check/',function(request,response,next){
+app.use('/check/', function(request, response, next) {
   jwt.verify(request.get('JWT'), jwt_secret, function(error, decoded) {
     if (error) {
       response.status(401).send('Unauthorized access');
     } else {
-      db.users.findOne({'_id': new mongojs.ObjectId(decoded._id)}, function(error, user) {
-        if (error){
+      db.users.findOne({
+        '_id': new mongojs.ObjectId(decoded._id)
+      }, function(error, user) {
+        if (error) {
           throw error;
-        }else{
-          if(user){
+        } else {
+          if (user) {
             next();
-          }else{
+          } else {
             response.status(401).send('Credentials are wrong.');
           }
         }
@@ -42,18 +44,20 @@ app.use('/check/',function(request,response,next){
   });
 })
 
-app.use('/admin/',function(request,response,next){
+app.use('/admin/', function(request, response, next) {
   jwt.verify(request.get('JWT'), jwt_admin, function(error, decoded) {
     if (error) {
       response.status(401).send('Unauthorized access');
     } else {
-      db.users.findOne({'_id': new mongojs.ObjectId(decoded._id)}, function(error, user) {
-        if (error){
+      db.users.findOne({
+        '_id': new mongojs.ObjectId(decoded._id)
+      }, function(error, user) {
+        if (error) {
           throw error;
-        }else{
-          if(user){
+        } else {
+          if (user) {
             next();
-          }else{
+          } else {
             response.status(401).send('Credentials are wrong.');
           }
         }
@@ -65,48 +69,46 @@ app.use('/admin/',function(request,response,next){
 app.post('/login', function(req, res) {
   var user = req.body;
   db.users.findOne({
-      'email': user.email,
+    'email': user.email,
   }, function(error, users) {
-      if (error) {
-          throw error;
-      }
-      if(users) {
-          bcrypt.compare(user.password, users.password, function(err, resp){
-              if(resp === true){
-                  if(users.type == "admin"){
-                    users.password = null;
-                      var token = jwt.sign(users, jwt_admin, {
-                          expiresIn: 60*60*24
-                      });
-                      res.send({
-                          success: true,
-                          message: 'Admin Authenticated',
-                          token: token,
-                          type : 'admin'
-                      })
-                      console.log("Admin authentication passed.");
-                  }
-                  else if(users.type == "user"){
-                    users.password = null;
-                      var token = jwt.sign(users, jwt_secret, {
-                          expiresIn: 60*60*24
-                      });
-                      res.send({
-                          success: true,
-                          message: 'Authenticated',
-                          token: token,
-                          type: "user"
-                      })
-                      console.log("Authentication passed.");
-                  }
-              }
-              else {
-                  res.send({
-                      user : false
-                  })
-              }
+    if (error) {
+      throw error;
+    }
+    if (users) {
+      bcrypt.compare(user.password, users.password, function(err, resp) {
+        if (resp === true) {
+          if (users.type == "admin") {
+            users.password = null;
+            var token = jwt.sign(users, jwt_admin, {
+              expiresIn: 60 * 60 * 24
+            });
+            res.send({
+              success: true,
+              message: 'Admin Authenticated',
+              token: token,
+              type: 'admin'
+            })
+            console.log("Admin authentication passed.");
+          } else if (users.type == "user") {
+            users.password = null;
+            var token = jwt.sign(users, jwt_secret, {
+              expiresIn: 60 * 60 * 24
+            });
+            res.send({
+              success: true,
+              message: 'Authenticated',
+              token: token,
+              type: "user"
+            })
+            console.log("Authentication passed.");
+          }
+        } else {
+          res.send({
+            user: false
           })
-      }
+        }
+      })
+    }
   });
 });
 
@@ -116,79 +118,79 @@ app.post('/register', function(req, res, next) {
   var user = req.body;
   var find = req.body.email;
   console.log(find);
-  bcrypt.hash(user.password, salt,null, function(err, hash) {
-    if(err){
+  bcrypt.hash(user.password, salt, null, function(err, hash) {
+    if (err) {
       throw err
     }
-      user.password = hash;
-      db.users.find({
-        email : find
-      }).toArray(function (err,result){
-        if(err) throw err;
+    user.password = hash;
+    db.users.find({
+      email: find
+    }).toArray(function(err, result) {
+      if (err) throw err;
 
-        console.log(result);
+      console.log(result);
 
-        if(result.length > 0){
-          res.sendStatus(204);
-        } else {
-          db.users.insert(user, function(err, data) {
-              if (err) return console.log(err);
-              res.setHeader('Content-Type', 'application/json');
-              res.send(user);
-          })
-        }
-      })
+      if (result.length > 0) {
+        res.sendStatus(204);
+      } else {
+        db.users.insert(user, function(err, data) {
+          if (err) return console.log(err);
+          res.setHeader('Content-Type', 'application/json');
+          res.send(user);
+        })
+      }
+    })
   })
 });
 
 
 
-app.get('/admin/tours', function (req, res) {
-  db.tour.find(function (err, docs) {
+app.get('/admin/tours', function(req, res) {
+  db.tour.find(function(err, docs) {
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.get('/check/tours', function (req, res) {
-  db.tour.find(function (err, docs) {
+app.get('/check/tours', function(req, res) {
+  db.tour.find(function(err, docs) {
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.post('/admin/tours', urlencodedParser, function (req, res, next) {
+app.post('/admin/tours', urlencodedParser, function(req, res, next) {
   console.log(req.body)
 
-  db.tour.insert(req.body, function (err, docs) {
+  db.tour.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.delete('/admin/deleteTour/:id', function (req, res) {
+app.delete('/admin/deleteTour/:id', function(req, res) {
   var id = req.params.id
   console.log(id)
   db.tour.remove({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('removed')
     res.json(doc)
   })
 })
 
-app.get('/admin/tours/:id', urlencodedParser, function (req, res) {
+app.get('/admin/tours/:id', urlencodedParser, function(req, res) {
   var id = req.params.id
   console.log(id)
   db.tour.findOne({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('selected')
     res.json(doc)
   })
 })
 
-app.put('/admin/tours/:id', function (req, res) {
+app.put('/admin/tours/:id', function(req, res) {
   var id = req.params.id
   console.log(req.body)
   db.tour.findAndModify({
@@ -204,52 +206,52 @@ app.put('/admin/tours/:id', function (req, res) {
       }
     },
     new: true
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('updated')
     res.json(doc)
   })
 })
 
 
-app.get('/admin/cars', function (req, res) {
-  db.cars.find(function (err, docs) {
+app.get('/admin/cars', function(req, res) {
+  db.cars.find(function(err, docs) {
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.post('/admin/cars', urlencodedParser, function (req, res, next) {
+app.post('/admin/cars', urlencodedParser, function(req, res, next) {
   console.log(req.body)
 
-  db.cars.insert(req.body, function (err, docs) {
+  db.cars.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.delete('/admin/deleteCars/:id', function (req, res) {
+app.delete('/admin/deleteCars/:id', function(req, res) {
   var id = req.params.id
   console.log(id)
   db.cars.remove({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('removed')
     res.json(doc)
   })
 })
 
-app.get('/admin/cars/:id', urlencodedParser, function (req, res) {
+app.get('/admin/cars/:id', urlencodedParser, function(req, res) {
   var id = req.params.id
   console.log(id)
   db.cars.findOne({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('selected')
     res.json(doc)
   })
 })
 
-app.put('/admin/cars/:id', function (req, res) {
+app.put('/admin/cars/:id', function(req, res) {
   var id = req.params.id
   console.log(req.body)
   db.cars.findAndModify({
@@ -265,51 +267,51 @@ app.put('/admin/cars/:id', function (req, res) {
       }
     },
     new: true
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('updated')
     res.json(doc)
   })
 })
 
-app.get('/admin/employees', function (req, res) {
-  db.employees.find(function (err, docs) {
+app.get('/admin/employees', function(req, res) {
+  db.employees.find(function(err, docs) {
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.post('/admin/employees', urlencodedParser, function (req, res, next) {
+app.post('/admin/employees', urlencodedParser, function(req, res, next) {
   console.log(req.body)
 
-  db.employees.insert(req.body, function (err, docs) {
+  db.employees.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.delete('/admin/deleteEmployees/:id', function (req, res) {
+app.delete('/admin/deleteEmployees/:id', function(req, res) {
   var id = req.params.id
   console.log(id)
   db.employees.remove({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('removed')
     res.json(doc)
   })
 })
 
-app.get('/admin/employees/:id', urlencodedParser, function (req, res) {
+app.get('/admin/employees/:id', urlencodedParser, function(req, res) {
   var id = req.params.id
   console.log(id)
   db.employees.findOne({
     _id: mongojs.ObjectId(id)
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('selected')
     res.json(doc)
   })
 })
 
-app.put('/admin/employees/:id', function (req, res) {
+app.put('/admin/employees/:id', function(req, res) {
   var id = req.params.id
   console.log(req.body)
   db.employees.findAndModify({
@@ -325,108 +327,134 @@ app.put('/admin/employees/:id', function (req, res) {
       }
     },
     new: true
-  }, function (err, doc) {
+  }, function(err, doc) {
     console.log('updated')
     res.json(doc)
   })
 })
 
-app.post('/tourFeedback/:id', urlencodedParser, function (req, res, next) {
+app.post('/tourFeedback/:id', urlencodedParser, function(req, res, next) {
   console.log(req.body)
   var id = req.params.id
-  console.log("server " +id)
+  console.log("server " + id)
   req.body.tour_id = id
-  db.tourFeedback.insert(req.body, function (err, docs) {
+  db.tourFeedback.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.post('/tourRequest/:id', urlencodedParser, function (req, res, next) {
+app.post('/tourRequest/:id', urlencodedParser, function(req, res, next) {
   console.log(req.body)
   var id = req.params.id
   console.log(id)
   req.body.tour_id = id
-  db.requestedTour.insert(req.body,  function (err, docs) {
+  db.requestedTour.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.post('/sendingAppFeed', urlencodedParser, function (req, res, next) {
+app.post('/sendingAppFeed', urlencodedParser, function(req, res, next) {
   console.log(req.body);
-  db.appFeedback.insert(req.body, function (err, docs) {
+  db.appFeedback.insert(req.body, function(err, docs) {
     console.log('inserted')
     res.json(docs)
   })
 })
 
-app.get('/registeredUser', urlencodedParser, function(req, res, next){
-  db.users.count(function(err, count){
+app.get('/registeredUser', urlencodedParser, function(req, res, next) {
+  db.users.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/toursNumber', urlencodedParser, function(req, res, next){
-  db.tour.count(function(err, count){
+app.get('/toursNumber', urlencodedParser, function(req, res, next) {
+  db.tour.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/tourFeedbackNumber', urlencodedParser, function(req, res, next){
-  db.tourFeedback.count(function(err, count){
+app.get('/tourFeedbackNumber', urlencodedParser, function(req, res, next) {
+  db.tourFeedback.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/appFeedbackNum', urlencodedParser, function(req, res, next){
-  db.appFeedback.count(function(err, count){
+app.get('/appFeedbackNum', urlencodedParser, function(req, res, next) {
+  db.appFeedback.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/tourRequestNum', urlencodedParser, function(req, res, next){
-  db.requestedTour.count(function(err, count){
+app.get('/tourRequestNum', urlencodedParser, function(req, res, next) {
+  db.requestedTour.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
 
-app.get('/carsNumber', urlencodedParser, function(req, res, next){
-  db.cars.count(function(err, count){
+app.get('/carsNumber', urlencodedParser, function(req, res, next) {
+  db.cars.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/employeesNumber', urlencodedParser, function(req, res, next){
-  db.employees.count(function(err, count){
+app.get('/employeesNumber', urlencodedParser, function(req, res, next) {
+  db.employees.count(function(err, count) {
     console.log(count)
     res.json(count)
   })
 })
 
-app.get('/admin/tourFeedback', function (req, res) {
-  db.tourFeedback.find(function (err, docs) {
+app.get('/admin/tourFeedback', function(req, res) {
+  // db.tourFeedback.find(function(err, docs) {
+  //   console.log(docs)
+  //   res.json(docs)
+  // })
+
+  db.tourFeedback.aggregate([{
+    $lookup: {
+      from: "tour",
+      localField: "tour_id",
+      foreignField: "_id",
+      as: "combinedResult"
+    }
+  }]).toArray(function(err,docs){
+    if(err) throw err;
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.get('/admin/appFeedback', function (req, res) {
-  db.appFeedback.find(function (err, docs) {
+app.get('/admin/appFeedback', function(req, res) {
+  db.appFeedback.find(function(err, docs) {
     console.log(docs)
     res.json(docs)
   })
 })
 
-app.get('/admin/tourRequest', function (req, res) {
-  db.requestedTour.find(function (err, docs) {
+app.get('/admin/tourRequest', function(req, res) {
+  // db.requestedTour.find(function (err, docs) {
+  //   console.log(docs)
+  //   res.json(docs)
+  // })
+
+  db.requestedTour.aggregate([{
+    $lookup: {
+      from: "tour",
+      localField: "tour_id",
+      foreignField: "_id",
+      as: "combinedResult"
+    }
+  }]).toArray(function(err,docs){
+    if(err) throw err;
     console.log(docs)
     res.json(docs)
   })
@@ -434,6 +462,6 @@ app.get('/admin/tourRequest', function (req, res) {
 
 //TODO: Spojit tour feedback i request sa tabelom tour i izbacit ime te ture preko ID
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Node app is running on port', port)
 })
